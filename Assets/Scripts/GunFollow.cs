e38321b8f01609d3d2f825d70114208797e2c9d5
nizhihuahome <116778012@qq.com>
2019-04-26 22:36:26 +0800

    完成了枪的制作

---
+++ Assets/Scripts/GunFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFollow : MonoBehaviour
+{
+    public Transform gunPos;
+
+    public Transform gun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       var mousePos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       //清除深度值
+       mousePos.z = 0;
+       Debug.Log(mousePos);
+       float z;
+       if (mousePos.x > gunPos.position.x)
+       {
+           z = -Vector3.Angle(Vector3.up, mousePos - gunPos.position);
+       }
+       else
+       {
+           z = Vector3.Angle(Vector3.up, mousePos - gunPos.position);
+       }
+       gun.localRotation=Quaternion.Euler(0,0,z);
+    }
+}
---
+++ Assets/Scripts/WaterWaveEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterWaveEffect : MonoBehaviour
+{
+    public Texture[] textures;
+
+    public Material mat;
+
+    public int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        InvokeRepeating("ChangeTexture",0,0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeTexture()
+    {
+        mat.mainTexture = textures[index];
+        if (index >= (textures.Length-1))
+        {
+            index = 0;
+        }
+        else
+        {
+            index++;
+        }
+    }
+}
