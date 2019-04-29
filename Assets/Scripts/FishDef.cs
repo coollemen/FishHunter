f7e598389fbb05a178204afd55bcab22c84c7d16
nizhihuahome <116778012@qq.com>
2019-04-29 22:11:44 +0800

    更新了odin插件，调整了鱼群生产算法

---
+++ Assets/Scripts/FishDef.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class FishDef
+{
+    public string url;
+    public int maxNum;
+    public int maxSpeed;
+    public float spawnTimeSpan;
+    public string prefabPath;
+}
--- Assets/Scripts/FishManager.cs
+++ Assets/Scripts/FishManager.cs
@@ -8,14 +8,18 @@ public class FishManager : MonoSingleton<FishManager>
 {
     public Transform[] fishCreatePositions;
 
-    public GameObject[] fishPrefabs;
-
+    public List<FishDef> fishDefs=new List<FishDef>();
     public float waveWaitTime = 1f;
 
     public float fishWaitTime = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < fishDefs.Count; i++)
+        {
+            PoolManager.Instance.CreatePool(fishDefs[i].url, fishDefs[i].prefabPath  , 10);
+        }
+        
         InvokeRepeating("MakeFishes",2,waveWaitTime );
     }
 
@@ -28,23 +32,24 @@ public class FishManager : MonoSingleton<FishManager>
     void MakeFishes()
     {
         int posIndex = Random.Range(0, fishCreatePositions.Length);
-        int fishIndex = Random.Range(0, fishPrefabs.Length);
-        var fishAtt = fishPrefabs[fishIndex].GetComponent<Fish>();
-        int maxNum = fishAtt.maxCount;
-        int maxSpeed = fishAtt.maxSpeed;
-        int num = Random.Range((maxNum/2)+1, maxNum);
-        int speed = Random.Range(maxSpeed / 2, maxSpeed);
-        StartCoroutine(CreateFish(fishIndex, posIndex, speed, num));
+        int fishIndex = Random.Range(0, fishDefs.Count);
+        var def = fishDefs[fishIndex];
+        int num = Random.Range((def.maxNum/2)+1, def.maxNum);
+        int speed = Random.Range(def.maxSpeed / 2, def.maxSpeed);
+        StartCoroutine(CreateFish(def.url, fishCreatePositions[posIndex],  num,speed,def.spawnTimeSpan));
         
     }
 
-    IEnumerator CreateFish(int prefabIndex,int posIndex, int speed,int count)
+    IEnumerator CreateFish(string url,Transform pos,int count,int speed,float timespan)
     {
         
         for (int i = 0; i < count; i++)
         {
-            GameObject go = Instantiate(fishPrefabs[prefabIndex],fishCreatePositions[posIndex]);
-            yield return new WaitForSeconds(fishWaitTime);
+            GameObject go = PoolManager.Instance.Spawn(url);
+            go.GetComponent<Fish>().speed = speed;
+            go.transform.position = pos.position;
+            go.transform.localRotation = pos.localRotation;
+            yield return new WaitForSeconds(timespan);
                 
         }
 
